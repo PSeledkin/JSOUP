532741129137a3f4a156c50369629d1ba569b7fb
aavtushko <p.seledkin@gmail.com>
2020-02-25 12:49:08 +0300

    test2

--- src/main/java/Seledkin/DuckDuckGoSearch.java
+++ src/main/java/Seledkin/DuckDuckGoSearch.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.File;
@@ -12,38 +11,24 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 class DuckDuckGoSearch {
     private final static String DUCKDUCKGO_SEARCH_URL = "https://duckduckgo.com/html/?q=";
 
-    private static String getSearchResults(String query) {
+    private static String getSearchResults(String query){
         Document doc ;
-        HashMap<String, String> res = new HashMap<>();
-        for (int i = 0; i < 2; i++) {
-            try {
-                if (i == 0) {
-                    doc = Jsoup.connect(DUCKDUCKGO_SEARCH_URL + query).timeout(5000).get();
-                } else {
-                    doc = Jsoup.connect(DUCKDUCKGO_SEARCH_URL + query + "&s=30&nextParams=&v=l&o=json&dc=30&api=%2Fd.js&kl=us-en").timeout(5000).get();
-                }
-                Elements results = doc.getElementById("links").getElementsByClass("results_links");
-                for (Element result : results) {
+        Map<String, String> res = null;
+        try {
 
-                    Element title = result.getElementsByClass("links_main").first().getElementsByTag("a").first();
-                    res.put(title.attr("href"), title.text());
+            doc = Jsoup.connect(DUCKDUCKGO_SEARCH_URL + query).timeout(5000).get();
 
-
-                    //           System.out.println("\nURL:" + title.attr("href"));
-                    //          System.out.println("Title:" + title.text());
-                    //         System.out.println(res);
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            Elements results = doc.getElementById("links").getElementsByClass("results_links");
+            res = results.stream().map(s -> s.getElementsByClass("links_main").first().getElementsByTag("a").first()).collect(Collectors.toMap(p -> p.attr("href"), t -> t.text()));
         }
+        catch (IOException e){e.printStackTrace();}
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         return gson.toJson(res);
 
@@ -71,6 +56,9 @@ class DuckDuckGoSearch {
 
     public static void main(String[] args) {
         String query = args[ 0 ];
+
         createFile(getSearchResults(query));
+
+
     }
 }
